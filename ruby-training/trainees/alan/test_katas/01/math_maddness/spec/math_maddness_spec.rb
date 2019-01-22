52140b2bba299a15b450e2ea450a372c79edb542
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 10:39:04 -0600

    Multiply method created and tested

--- ruby-training/trainees/alan/test_katas/01/math_maddness/spec/math_maddness_spec.rb
+++ ruby-training/trainees/alan/test_katas/01/math_maddness/spec/math_maddness_spec.rb
@@ -5,6 +5,14 @@ class Aritmetic_operations
     def self.add(first_number, second_number)
        first_number +  second_number
     end
+    
+    def self.substraction(first_number, second_number)
+        first_number - second_number
+    end
+
+    def self.multiply(first_number, second_number)
+        first_number * second_number
+    end
 end
 describe Aritmetic_operations do
     describe "#add" do
@@ -13,4 +21,19 @@ describe Aritmetic_operations do
             expect(calculus).to eq(16)
         end
     end
+
+    describe "#substraction" do
+        it "The first number is substracted by the second number, the result is 10" do
+            calculus = Aritmetic_operations.substraction(15,5)
+            expect(calculus).to eq(10)
+        end
+    end
+
+    describe "#multiply" do
+        it "The result, that is 48, is the multiply of the two numbers" do
+            calculus = Aritmetic_operations.multiply(8,6)
+            expect(calculus).to eq(48)
+        end
+    end
 end
+
