d897571757f2e573cb4476d95dac0f18a5961046
sbog8 <shareni.ortega@udem.edu>
2019-01-22 12:21:03 -0600

    Returns Fizz or Buzz if divisible by 3 or 5 respectively or Fizzbuzz if divisible by both

---
+++ ruby-training/trainees/shareni/test_katas/02/fizzbuzz_game/fizzbuzz.rb
@@ -0,0 +1,11 @@
+class FizzBuzz
+    def play(number)
+        if number % 3 == 0 and number % 5 == 0
+            "FizzBuzz"
+        elsif number % 3 == 0
+            "Fizz"
+        elsif number % 5 == 0
+            "Buzz"
+        end
+    end
+end
