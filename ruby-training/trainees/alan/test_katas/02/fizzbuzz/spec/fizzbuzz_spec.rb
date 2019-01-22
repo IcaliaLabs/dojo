6b3a6e7c1a0bdd9fed33659a028c49fced8dcbfe
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 14:24:57 -0600

    Added method for looking buzz  and tested

--- ruby-training/trainees/alan/test_katas/02/fizzbuzz/spec/fizzbuzz_spec.rb
+++ ruby-training/trainees/alan/test_katas/02/fizzbuzz/spec/fizzbuzz_spec.rb
@@ -4,9 +4,16 @@ require '../fizzbuzz_game.rb'
 describe FizzBuzzGame do
 
     describe ".it_is_fizz?" do
-        it "should recieve true if the number is a fizz" do
+        it "should return true if the number is divisible by 3" do
             expect(FizzBuzzGame.it_is_fizz?(6)).to eq(true)
         end
 
     end
+
+    describe ".it_is_buzz?" do
+        it "should return true if the number is divisible by 5" do
+            expect(FizzBuzzGame.it_is_buzz?(15)).to eq(true)
+        end
+    end
+
 end
