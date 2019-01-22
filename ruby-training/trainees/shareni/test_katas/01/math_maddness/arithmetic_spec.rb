6bb240e88bc365b6893be490a68366ced00a469b
sbog8 <shareni.ortega@udem.edu>
2019-01-22 11:15:16 -0600

    Use operand instead of number

--- ruby-training/trainees/shareni/test_katas/01/math_maddness/arithmetic_spec.rb
+++ ruby-training/trainees/shareni/test_katas/01/math_maddness/arithmetic_spec.rb
@@ -5,7 +5,7 @@ class Calculator
         first_operand + second_operand
     end
 
-    def subtract(first_operand, second_operand)
+    def substract(first_operand, second_operand)
         first_operand - second_operand
     end
 
@@ -23,41 +23,36 @@ class Calculator
 end
 
 describe "Calculator" do
-    describe "#add" do
-        it "adds two numbers" do
-            calculator = Calculator.new
-            result = calculator.add(5, 4)
-            expect(result).to eq(9)
-        end
+    it "adds two numbers" do
+        calculator = Calculator.new
+        result = calculator.add(5,4)
+        expect(result).to eq(9)
     end
 
-    describe "#subtract" do
-        it "subtracts two numbers" do
-            calculator = Calculator.new
-            result = calculator.subtract(4, 5)
-            expect(result).to eq(-1)
-        end
+    it "substracts two numbers" do
+        calculator = Calculator.new
+        result = calculator.substract(4,5)
+        expect(result).to eq(-1)
     end
 
-    describe "#multiply" do
-        it "multiplies two numbers" do
-            calculator = Calculator.new
-            result = calculator.multiply(5, 6)
-            expect(result).to eq(30)
-        end
+    it "multiplies two numbers" do
+        calculator = Calculator.new
+        result = calculator.multiply(5,6)
+        expect(result).to eq(30)
     end
 
     describe "#divide" do
         it "divides two numbers that aren't 0" do
             calculator = Calculator.new
-            result = calculator.divide(10, 5)
+            result = calculator.divide(10,5)
             expect(result).to eq(2)
         end
 
-        it "shows an error message if the denominator is 0" do
+        it "shows a message if the divider is 0" do
             calculator = Calculator.new
-            result = calculator.divide(5, 0)
+            result = calculator.divide(5,0)
             expect(result).to eq("Can't divide by 0")
         end
+
     end
 end
