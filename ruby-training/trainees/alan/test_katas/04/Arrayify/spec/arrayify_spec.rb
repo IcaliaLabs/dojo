8e875efa8624fd8d4ac8cffc83362178619553da
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 17:27:49 -0600

    Added make it reverse method and tested

--- ruby-training/trainees/alan/test_katas/04/Arrayify/spec/arrayify_spec.rb
+++ ruby-training/trainees/alan/test_katas/04/Arrayify/spec/arrayify_spec.rb
@@ -2,9 +2,15 @@ require 'rspec/autorun'
 require '../arrayify.rb'
 
 describe Arrayify do
-        describe ".to_a_digits_array" do
-            it "should return an array of digits of the number" do
+    describe ".to_a_digits_array" do
+        it "should return an array of digits of the number" do            
             expect(Arrayify.to_a_digits_array(1235)).to match_array([1,2,3,5])
+        end
+    end
+
+    describe ".make_it_reverse" do
+        it "should return a reverse array from the array" do
+            expect(Arrayify.make_it_reverse(1235)).to match_array([5,3,2,1])
         end
     end
 end
