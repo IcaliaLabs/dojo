2a98fac38c82d8936b7645375fd5a3cd14b8a765
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 16:15:14 -0600

    Added capitalized_word! method.

--- ruby-training/trainees/alan/test_katas/03/JadenCase/spec/jaden_case_spec.rb
+++ ruby-training/trainees/alan/test_katas/03/JadenCase/spec/jaden_case_spec.rb
@@ -8,4 +8,10 @@ describe JadenCase do
             expect(JadenCase.to_array_of_words("Hello, there.")).to eq(["Hello,","there."])
         end
     end
+
+    describe ".capitalize_words" do
+        it "should return an array of capitalized words" do
+            expect(JadenCase.capitalize_words!("Hello, there.")).to eq(["Hello,","There."])
+        end
+    end
 end
