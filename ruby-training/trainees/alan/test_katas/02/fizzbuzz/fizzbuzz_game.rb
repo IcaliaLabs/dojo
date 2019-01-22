0fdbb304bea986f600ad2d437d87f1b1f872c927
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 15:24:03 -0600

    Finals details

--- ruby-training/trainees/alan/test_katas/02/fizzbuzz/fizzbuzz_game.rb
+++ ruby-training/trainees/alan/test_katas/02/fizzbuzz/fizzbuzz_game.rb
@@ -15,4 +15,20 @@ class FizzBuzzGame
     def self.it_has_a_3? (number)
         number.to_s.include?("3")
     end
+
+    def self.it_has_a_5? (number)
+        number.to_s.include?("5")
+    end
+
+    def self.game(number)
+        if it_is_fizzbuzz?(number)
+            "FizzBuzz"
+        elsif it_is_buzz?(number) or it_has_a_5?(number)
+            "Buzz"
+        elsif it_is_fizz?(number) or it_has_a_3?(number)
+            "Fizz"
+        else 
+            number
+        end
+    end
 end
