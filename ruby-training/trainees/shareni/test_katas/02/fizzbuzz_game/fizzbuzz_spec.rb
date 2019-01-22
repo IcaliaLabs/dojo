46c48d4a2e59e4937c2bbba60a8df71b162db0e2
sbog8 <shareni.ortega@udem.edu>
2019-01-22 15:17:11 -0600

    Use describe blocks for context

--- ruby-training/trainees/shareni/test_katas/02/fizzbuzz_game/fizzbuzz_spec.rb
+++ ruby-training/trainees/shareni/test_katas/02/fizzbuzz_game/fizzbuzz_spec.rb
@@ -3,40 +3,46 @@ require './fizzbuzz'
 
 describe "FizzBuzz" do
     describe "#play" do
-        it "returns Fizz if the number is divisible by 3" do
-            game = FizzBuzz.new
-            result = game.play(6)
-            expect(result).to eq("Fizz")
+        describe "Fizz" do
+            it "if the number is divisible by 3" do
+                game = FizzBuzz.new
+                result = game.play(6)
+                expect(result).to eq("Fizz")
+            end
+
+            it "if the number has a 3" do
+                game = FizzBuzz.new
+                result = game.play(13)
+                expect(result).to eq("Fizz")
+            end
         end
 
-        it "returns Fizz if the number has a 3" do
-            game = FizzBuzz.new
-            result = game.play(13)
-            expect(result).to eq("Fizz")
+        describe "buzz" do
+            it "if the number is divisible by 5" do
+                game = FizzBuzz.new
+                result = game.play(10)
+                expect(result).to eq("Buzz")
+            end 
+
+            it "if the number has a 5" do
+                game = FizzBuzz.new
+                result = game.play(52)
+                expect(result).to eq("Buzz")
+            end 
         end
 
-        it "returns Buzz if the number is divisible by 5" do
-            game = FizzBuzz.new
-            result = game.play(10)
-            expect(result).to eq("Buzz")
-        end 
+        describe "FizzBuzz" do
+            it "returns FizzBuzz if the number is divisible by 3 and 5" do
+                game = FizzBuzz.new
+                result = game.play(15)
+                expect(result).to eq("FizzBuzz") 
+            end
 
-        it "returns Buzz if the number has a 5" do
-            game = FizzBuzz.new
-            result = game.play(52)
-            expect(result).to eq("Buzz")
-        end 
-
-        it "returns FizzBuzz if the number is divisible by 3 and 5" do
-           game = FizzBuzz.new
-           result = game.play(15)
-           expect(result).to eq("FizzBuzz") 
-        end
-
-        it "returns the same number if it's not divisible by 3, 5 or both" do
-            game = FizzBuzz.new
-            result = game.play(8)
-            expect(result).to eq(8)
+            it "returns the same number if it's not divisible by 3, 5 or both" do
+                game = FizzBuzz.new
+                result = game.play(8)
+                expect(result).to eq(8)
+            end
         end
     end
 end
