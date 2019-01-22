26378bb03c11959ecfe2481f47bd6ff7ddb990f1
Alan Rocha <alan.rocha@udem.edu>
2019-01-22 17:17:45 -0600

    Added method to convert int to an array

--- ruby-training/trainees/alan/test_katas/04/Arrayify/spec/arrayify_spec.rb
+++ ruby-training/trainees/alan/test_katas/04/Arrayify/spec/arrayify_spec.rb
@@ -1,1 +1,10 @@
 require 'rspec/autorun'
+require '../arrayify.rb'
+
+describe Arrayify do
+        describe ".to_a_digits_array" do
+            it "should return an array of digits of the number" do
+            expect(Arrayify.to_a_digits_array(1235)).to match_array([1,2,3,5])
+        end
+    end
+end
