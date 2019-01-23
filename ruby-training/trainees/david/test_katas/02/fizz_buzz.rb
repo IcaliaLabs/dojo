b0f4cfecba89c01c2cf82e5e8172de2e1e08e4dc
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-23 15:44:47 -0600

    finished test 02

--- ruby-training/trainees/david/test_katas/02/fizz_buzz.rb
+++ ruby-training/trainees/david/test_katas/02/fizz_buzz.rb
@@ -1,5 +1,9 @@
 class FizzBuzz
 
-    def play(num)
+    def self.play(num)
+        return "fizzbuzz" if num % 3 == 0 and num % 5 == 0
+        return "fizz" if num % 3 == 0 or num.to_s.include?("#{3}")
+        return "buzz" if num % 5 == 0 or num.to_s.include?("#{5}")
+        num
     end
 end
