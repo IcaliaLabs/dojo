554f5e29a53d35ae50bea59881650e6d81110857
Alan Rocha <alan.rocha@udem.edu>
2019-01-23 15:31:56 -0600

    Corrected and refactored sort_array method. Added eliminate_min_and_max method and tested.

--- ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/spec/sum_with_out_max_min_spec.rb
+++ ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/spec/sum_with_out_max_min_spec.rb
@@ -3,10 +3,15 @@ require '../sum_with_out_max_min.rb'
 
 describe SumWithOutMaxMin do
 
-    describe ".sort_array" do
+    describe ".sort_array_of_intergers" do
         it "should return a sorted array from smallest to largest value" do
-            expect([3,6,9,4]).to match_array([3,4,6,9])
+            expect(SumWithOutMaxMin.sort_array_of_intergers([6,4,9,3])).to match_array([3,4,6,9])
         end
     end
 
+    describe ".eliminate_min_and_max" do
+        it "should return an array without the first and last element" do
+            expect(SumWithOutMaxMin.eliminate_min_and_max([6,4,9,3])).to match_array([4,6])
+        end
+    end
 end
