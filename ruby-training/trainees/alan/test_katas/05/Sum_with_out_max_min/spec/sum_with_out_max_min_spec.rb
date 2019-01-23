8faaf019edf85de4adacda056b91232367e44d2b
Alan Rocha <alan.rocha@udem.edu>
2019-01-23 15:01:56 -0600

    Added sort_array method and tested

--- ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/spec/sum_with_out_max_min_spec.rb
+++ ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/spec/sum_with_out_max_min_spec.rb
@@ -1,2 +1,12 @@
 require 'rspec/autorun'
+require '../sum_with_out_max_min.rb'
 
+describe SumWithOutMaxMin do
+
+    describe ".sort_array" do
+        it "should return a sorted array from smallest to largest value" do
+            expect([3,6,9,4]).to match_array([3,4,6,9])
+        end
+    end
+
+end
