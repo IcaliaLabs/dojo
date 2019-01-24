5d3e49edb92b3ddb53c18ed22f2061e4a840afe5
Alan Rocha <alan.rocha@udem.edu>
2019-01-24 12:27:34 -0600

    Changed name methods

--- ruby-training/trainees/alan/test_katas/02/fizzbuzz/fizzbuzz_game.rb
+++ ruby-training/trainees/alan/test_katas/02/fizzbuzz/fizzbuzz_game.rb
@@ -1,31 +1,31 @@
 class FizzBuzzGame
     
-    def self.it_is_fizz? (number)
+    def self.is_it_fizz? (number)
         number % 3 == 0
     end
 
-    def self.it_is_buzz? (number)
+    def self.is_it_buzz? (number)
         number % 5 == 0
     end
 
-    def self.it_is_fizzbuzz? (number)
-        it_is_buzz?(number) and it_is_fizz?(number)
+    def self.is_it_fizzbuzz? (number)
+        is_it_buzz?(number) and is_it_fizz?(number)
     end
 
-    def self.it_has_a_3? (number)
+    def self.has_it_a_3? (number)
         number.to_s.include?("3")
     end
 
-    def self.it_has_a_5? (number)
+    def self.has_it_a_5? (number)
         number.to_s.include?("5")
     end
 
     def self.game(number)
-        if it_is_fizzbuzz?(number)
+        if is_it_fizzbuzz?(number)
             "FizzBuzz"
-        elsif it_is_buzz?(number) or it_has_a_5?(number)
+        elsif is_it_buzz?(number) or has_it_a_5?(number)
             "Buzz"
-        elsif it_is_fizz?(number) or it_has_a_3?(number)
+        elsif is_it_fizz?(number) or has_it_a_3?(number)
             "Fizz"
         else 
             number
--- ruby-training/trainees/alan/test_katas/02/fizzbuzz/spec/fizzbuzz_spec.rb
+++ ruby-training/trainees/alan/test_katas/02/fizzbuzz/spec/fizzbuzz_spec.rb
@@ -3,34 +3,34 @@ require '../fizzbuzz_game.rb'
 
 describe FizzBuzzGame do
 
-    describe ".it_is_fizz?" do
+    describe ".is_it_fizz?" do
         it "should return true if the number is divisible by 3" do
-            expect(FizzBuzzGame.it_is_fizz?(6)).to eq(true)
+            expect(FizzBuzzGame.is_it_fizz?(6)).to eq(true)
         end
 
     end
 
-    describe ".it_is_buzz?" do
+    describe ".is_it_buzz?" do
         it "should return true if the number is divisible by 5" do
-            expect(FizzBuzzGame.it_is_buzz?(20)).to eq(true)
+            expect(FizzBuzzGame.is_it_buzz?(20)).to eq(true)
         end
     end
 
-    describe ".it_is_fizzbuzz?" do
+    describe ".is_it_fizzbuzz?" do
         it "should return true if the number is divisible by 5 and 3" do
-            expect(FizzBuzzGame.it_is_fizzbuzz?(15)).to eq(true)
+            expect(FizzBuzzGame.is_it_fizzbuzz?(15)).to eq(true)
         end
     end
 
-    describe ".it_has_a_3?" do
+    describe ".has_it_a_3?" do
         it "should return true if the number has a 3" do
-            expect(FizzBuzzGame.it_has_a_3? (531)).to eq(true)
+            expect(FizzBuzzGame.has_it_a_3? (531)).to eq(true)
         end
     end
 
-    describe ".it_has_a_5?" do
+    describe ".has_it_a_5?" do
         it "should return true if the number has a 5" do
-            expect(FizzBuzzGame.it_has_a_5? (1531)).to eq(true)
+            expect(FizzBuzzGame.has_it_a_5? (1531)).to eq(true)
         end
     end
     
