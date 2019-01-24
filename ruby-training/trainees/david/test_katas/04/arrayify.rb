d43826db86e18e17105171075a0fecdef952d6f8
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-24 10:08:40 -0600

    Test 04 first attempt

--- ruby-training/trainees/david/test_katas/04/arrayify.rb
+++ ruby-training/trainees/david/test_katas/04/arrayify.rb
@@ -1,5 +1,6 @@
 class Arrayify
 
     def self.to_inverse_array(number)
+        number.to_s.reverse.chars.map(&:to_i)
     end
 end
--- ruby-training/trainees/david/test_katas/04/spec/arrayify_spec.rb
+++ ruby-training/trainees/david/test_katas/04/spec/arrayify_spec.rb
@@ -1,7 +1,10 @@
 require 'rspec/autorun'
-require_relative '../jaden'
+require_relative '../arrayify'
 
 describe Arrayify do
     describe ".to_inverse_array" do
+        it "should return an inverted array of integers" do
+            expect(Arrayify.to_inverse_array(453622)).to eq([2,2,6,3,5,4])
+        end
     end
 end
