cc2c6bfab712424e21d219c2128e4a1b3b08849b
Alan Rocha <alan.rocha@udem.edu>
2019-01-24 15:21:42 -0600

    Resolve parentheses issue

--- ruby-training/trainees/alan/test_katas/03/JadenCase/jaden_case.rb
+++ ruby-training/trainees/alan/test_katas/03/JadenCase/jaden_case.rb
@@ -2,15 +2,15 @@
 
 class JadenCase
     
-    def self.to_array_of_words (sentence)
+    def self.to_array_of_words(sentence)
         sentence.split()
     end
 
-    def self.capitalize_words! (sentence)
+    def self.capitalize_words!(sentence)
         to_array_of_words(sentence).each { |word| word.capitalize!}
     end
 
-    def self.make_it_in_jaden_case (sentence)
+    def self.make_it_in_jaden_case(sentence)
         capitalize_words!(sentence).join(" ")
     end
 end
--- ruby-training/trainees/alan/test_katas/04/Arrayify/arrayify.rb
+++ ruby-training/trainees/alan/test_katas/04/Arrayify/arrayify.rb
@@ -1,12 +1,12 @@
 
 
 class Arrayify
-    def self.to_a_digits_array (number)
+    def self.to_a_digits_array(number)
         array_of_digits = number.to_s.split("")
         array_of_digits.map(&:to_i)
     end
 
-    def self.make_it_reverse (number)
+    def self.make_it_reverse(number)
         to_a_digits_array(number).reverse
     end
 end
