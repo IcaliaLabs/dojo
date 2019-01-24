f0878db53f6ad4bea5a278a444141272ffc1b996
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-24 10:58:11 -0600

    test 06. refactored code

--- ruby-training/trainees/david/test_katas/06/string_calculator.rb
+++ ruby-training/trainees/david/test_katas/06/string_calculator.rb
@@ -3,7 +3,6 @@ class StringCalculator
     def self.add(string_numbers)
         return 0 if string_numbers.empty?
         numbers = string_numbers.split(",").map(&:to_i)
-        return numbers.first if numbers.size < 2
-        return numbers.reduce(:+) if numbers.size > 1
+        return numbers.reduce(:+)
     end
 end
