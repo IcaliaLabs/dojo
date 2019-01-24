eb10e035d58c783f4915c2260654f6c96a0a9210
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-24 09:53:31 -0600

    added class method for test 04

---
+++ ruby-training/trainees/david/test_katas/04/arrayify.rb
@@ -0,0 +1,5 @@
+class Arrayify
+
+    def self.to_inverse_array(number)
+    end
+end
---
+++ ruby-training/trainees/david/test_katas/04/spec/arrayify_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec/autorun'
+require_relative '../jaden'
+
+describe Arrayify do
+    describe ".to_inverse_array" do
+    end
+end
