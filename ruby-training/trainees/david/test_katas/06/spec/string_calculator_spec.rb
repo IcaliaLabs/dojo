0eebd262c2f97dc098319169be772944bb8395d7
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-24 10:53:15 -0600

    the second case was tested and passed

--- ruby-training/trainees/david/test_katas/06/spec/string_calculator_spec.rb
+++ ruby-training/trainees/david/test_katas/06/spec/string_calculator_spec.rb
@@ -6,5 +6,8 @@ describe StringCalculator do
         it "should return 0 if string is empty" do
             expect(StringCalculator.add("")).to eq(0)
         end
+        it "should return the string value in integer value in case the string only contains one number" do
+            expect(StringCalculator.add("3")).to eq(3)
+        end
     end
 end
--- ruby-training/trainees/david/test_katas/06/string_calculator.rb
+++ ruby-training/trainees/david/test_katas/06/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
 
     def self.add(string_numbers)
         return 0 if string_numbers.empty?
+        numbers = string_numbers.split(",").map(&:to_i)
+        return numbers.first if numbers.size < 2
     end
 end
