d2ffd2eb92d3a03adda0fa267b914cadede9a078
sbog8 <shareni.ortega@udem.edu>
2019-01-25 12:27:06 -0600

    Tests of special cases approved

--- ruby-training/trainees/shareni/test_katas/05/sum_array/array_spec.rb
+++ ruby-training/trainees/shareni/test_katas/05/sum_array/array_spec.rb
@@ -8,5 +8,17 @@ describe "Array" do
            result = example_array.sum_without_max_and_min
            expect(result).to eq(10)
         end
+
+        it "returns the sum of the array without one minimum if there are more than one minimums" do
+            example_array = NewArray.new([1,2,10,8,10])
+            result = example_array.sum_without_max_and_min
+            expect(result).to eq(20)
+        end
+
+        it "returns the sum of the array without one maximum if there are more than one maximums" do
+            example_array = NewArray.new([1,2,1,8,1,10])
+            result = example_array.sum_without_max_and_min
+            expect(result).to eq(12)
+        end
     end
 end
