14e564073cabf33058546d0e31df2d029822223f
David Cantu <davidcantu@Davids-MacBook-Pro-2.local>
2019-01-28 10:50:04 -0600

    Test exercise 8 all tests pass

--- ruby-training/trainees/david/test_katas/08/lib/playing_card.rb
+++ ruby-training/trainees/david/test_katas/08/lib/playing_card.rb
@@ -1,6 +1,11 @@
 class PlayingCard
   attr_accessor :suit, :value
 
+  def initialize(value, suit)
+    @suit = suit
+    @value = value
+  end
+
   def inspect
     "#{value.to_s} of #{suit.to_s}"
   end
--- ruby-training/trainees/david/test_katas/08/lib/playing_card_deck.rb
+++ ruby-training/trainees/david/test_katas/08/lib/playing_card_deck.rb
@@ -4,10 +4,24 @@ require_relative './playing_card'
 class PlayingCardDeck < Deck
   def initialize
     super
-    
+    @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
+    @suits = ['clubs', 'hearts', 'spades', 'diamonds']
+ 
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        card = PlayingCard.new(rank,suit)
+        @cards << card
+      end
+    end
+
+    shuffle
   end
 
   def deal_poker_hands(count)
-    # your code here
+    poker_hands = (1..count).map { |hand| [] }
+    (1..5).each do |card|
+      poker_hands.each { |hand| hand << @cards.pop }
+    end
+    poker_hands
   end
 end
