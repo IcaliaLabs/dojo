4879e1bd6e480c1f3d70a9f61a232f492813b7ac
sbog8 <shareni.ortega@udem.edu>
2019-01-28 14:39:44 -0600

    Spelling errors in describe NewArray block

--- ruby-training/trainees/shareni/test_katas/05/sum_array/array_spec.rb
+++ ruby-training/trainees/shareni/test_katas/05/sum_array/array_spec.rb
@@ -1,21 +1,21 @@
 require 'rspec'
 require_relative 'new_array'
 
-describe "Array" do
-    describe "#sum" do
-        it "returns the sum of the array without the maximum and minimum value" do
+describe "NewArray" do
+    describe "#sum_without_max_and_min" do
+        it "should return the sum of the array without the maximum and minimum value" do
            example_array = NewArray.new([1,2,8,10])
            result = example_array.sum_without_max_and_min
            expect(result).to eq(10)
         end
 
-        it "returns the sum of the array without one minimum if there are more than one minimums" do
+        it "should return the sum of the array without one minimum if there is more than one minimum" do
             example_array = NewArray.new([1,2,10,8,10])
             result = example_array.sum_without_max_and_min
             expect(result).to eq(20)
         end
 
-        it "returns the sum of the array without one maximum if there are more than one maximums" do
+        it "should return the sum of the array without one maximum if there is more than one maximum" do
             example_array = NewArray.new([1,2,1,8,1,10])
             result = example_array.sum_without_max_and_min
             expect(result).to eq(12)
