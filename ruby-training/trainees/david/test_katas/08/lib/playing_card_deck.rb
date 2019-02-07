6f7c1d3d621f8cfdfc48ba4e6106e47fc78620dd
David Cantu Martinez <davidcantu966@gmail.com>
2019-02-07 11:35:47 -0600

    Refactor dealing hands

--- ruby-training/trainees/david/test_katas/08/lib/playing_card_deck.rb
+++ ruby-training/trainees/david/test_katas/08/lib/playing_card_deck.rb
@@ -4,21 +4,41 @@ require_relative './playing_card'
 class PlayingCardDeck < Deck
   def initialize
     super
-    @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
-    @suits = ['clubs', 'hearts', 'spades', 'diamonds']
- 
-    @ranks.each do |rank|
-      @suits.each do |suit|
-        @cards << PlayingCard.new(rank,suit)
-      end
-    end
-
+    assign_ranks
+    assign_suits
+    populate_deck
     shuffle
   end
 
   def deal_poker_hands(count)
-    poker_hands = (1..count).map { |hand| [] }
-    (1..5).each do |card|
+    poker_hands = create_empty_hands(count)
+    deal_cards_to(poker_hands)
+  end
+
+  private
+
+  def assign_ranks
+    @ranks = [*(2..10), 'J', 'Q', 'K', 'A']
+  end
+
+  def assign_suits
+    @suits = ['clubs', 'hearts', 'spades', 'diamonds']
+  end
+
+  def populate_deck
+    @suits.each do |suit|
+      @ranks.each do |rank|
+        @cards << PlayingCard.new(rank,suit)
+      end
+    end
+  end
+
+  def create_empty_hands(hands)
+    (1..hands).map { |hand| [] }
+  end
+
+  def deal_cards_to(poker_hands)
+    5.times do
       poker_hands.each { |hand| hand << @cards.pop }
     end
     poker_hands
