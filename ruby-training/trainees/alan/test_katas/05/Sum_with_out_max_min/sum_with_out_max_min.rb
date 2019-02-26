400dca9f4d52a5e6cc3923dcfc0d7349f8454630
Alan Rocha <alan.rocha@udem.edu>
2019-02-26 10:16:51 -0600

    Refactored methods

--- ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/sum_with_out_max_min.rb
+++ ruby-training/trainees/alan/test_katas/05/Sum_with_out_max_min/sum_with_out_max_min.rb
@@ -1,12 +1,8 @@
-
 class SumWithOutMaxMin
-    
-   
+
     def self.remove_min_and_max(array_of_intergers)
-        sorted_array = array_of_integers.sort
-        sorted_array.shift
-        sorted_array.pop
-        return sorted_array
+        sorted_array = array_of_intergers.sort
+        return sorted_array[1..-2]
     end
 
     def self.sum_array(array_of_intergers)
