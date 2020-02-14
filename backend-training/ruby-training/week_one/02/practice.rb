26e0b38708cd7f7f35bfef5a2500cee8d8addc1a
Sandra Gonzalez Diaz <sandry0488@hotmail.com>
2020-02-14 12:33:50 -0600

    Solution for exercise 2

--- backend-training/ruby-training/week_one/02/practice.rb
+++ backend-training/ruby-training/week_one/02/practice.rb
@@ -37,6 +37,59 @@ REVIEWEES = [
 
 ## Your script starts here
 
+REVIEWERS = [
+  { name: "Walter" },
+  { name: "Vov" },
+  { name: "Cejas" },
+  { name: "Kuri" },
+  { name: "Sherwell" },
+]
+
+REVIEWEES = [
+{ name: "Barbara", email: "bmbrina@icalialabs.com" },
+{ name: "Hector", email: "hector@icalialabs.com" },
+{ name: "Kevin", email: "kevin@icalialabs.com" },
+{ name: "Julian", email: "julz@icalialabs.com" },
+{ name: "Luis Gerardo", email: "gvaldez@icalialabs.com" },
+{ name: "Cesar Nava", email: "cnava@icalialabs.com" },
+]
+
+# Devuelve aleatoriamente una cantidad de elementos de los datos
+def delvolverRevisores(cantidad, listado)
+seleccionados = []
+
+while seleccionados.size < cantidad do
+  index = rand(listado.size)
+  seleccionados << listado[index]
+  seleccionados.uniq!
+end
+
+seleccionados
+end
+
+# Devuelve el texto del email
+def prepararCorreoElectronico(nombre, email, revisores)
+"E-mail to: #{email}
+        Body:
+
+        Hi, #{nombre}.
+        Your reviewers are #{revisores[0][:name]} and #{revisores[1][:name]}.
+        "
+end
+
+# Imprime el email
+def mandarEmailsDeListado(listado_alumnos)
+listado_alumnos.each do |alumno|
+  revisores = delvolverRevisores(2, REVIEWERS)
+  nombre_alumno = alumno[:name]
+  email_alumno = alumno[:email]
+
+  puts prepararCorreoElectronico(nombre_alumno, email_alumno, revisores)
+end
+end
+
+# Ejecutar metodo - Mandar emails
+mandarEmailsDeListado(REVIEWEES)
 
 
 ## Your script ends here
