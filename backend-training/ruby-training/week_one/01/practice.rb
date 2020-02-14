3ec203ad55c9e3632b9ac4a48ef7419a717dd5b2
Sandra Gonzalez Diaz <sandry0488@hotmail.com>
2020-02-14 12:15:16 -0600

    Solution for exercise 1

--- backend-training/ruby-training/week_one/01/practice.rb
+++ backend-training/ruby-training/week_one/01/practice.rb
@@ -4,12 +4,26 @@
 # 2. Store the age in the hash as an integer
 # 3. Add a skills array to each developer on the room
 # 4. Add a friends key to each developer with a hash describing each friend (name)
-# 5. Strip trailing chars of input, look for the appropriate function here:
-# http://ruby-doc.org/core-2.4.1/String.html
+# 5. Strip trailing chars of input, look for the appropriate function here
 # 6. Print the developer profile matching the following format:
 #  Abraham Kuri is a developer of 29 years old with 3 friends and is able to cook, do magic, ride a bike
 
 # Your script starts here
+developer = {}
+
+puts "What is your name?"
+developer[:name] = gets.chomp
+
+puts "How old are you?"
+developer[:age] = gets.chomp.to_i
+
+puts "Tell me the names of all your friends"
+developer[:friends] = gets.chomp.split(',')
+
+puts "Write your skills separated by comma"
+developer[:skills] = gets.chomp.split(',')
+
+puts "#{developer[:name]} is a developer of #{developer[:age]} years old with #{developer[:friends].size} friends and is able to #{developer[:skills].join(", ")}"
 
 # Your script ends here
 
